92a4254991e339870e77e787bfd21fbd53db2bd7
Zineddine <zinedinetadjer1@gmail.com>
2024-01-24 14:31:57 +0100

    header creation

--- cs/src/components/navbar/Navbar.jsx
+++ cs/src/components/navbar/Navbar.jsx
@@ -6,7 +6,13 @@ export const Navbar = () => {
   return (
     <div id='nav'>
         <img src={Logo} alt="logo UPS" className='logo' />
+      <div id='nav1'>
         <h1>UPS Algerie Costumer Service</h1>
+      </div>
+        <div className='links'>
+        <a href="#"><h3>Tarifs</h3></a>
+        <a href="#"><h3>Communes desservies</h3></a>
+        </div>
     </div>
   )
 }
